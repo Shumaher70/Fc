cc99faac9ff6d14dec42d7191bb684f6bdac4e46
Oleg Vrabiy <olegvraby@gmail.com>
2024-06-05 19:42:07 +0200

    create:Cursor: The component for render current cursor position

---
+++ components/cursor/Cursor.tsx
@@ -0,0 +1,23 @@
+import CursorSVG from "@/public/assets/CursorSVG";
+
+type Props = {
+  color: string;
+  x: number;
+  y: number;
+  message: string;
+};
+
+const Cursor = ({ color, x, y, message }: Props) => {
+  return (
+    <div
+      className="pointer-events-none absolute left-0 top-0"
+      style={{ transform: `translateX(${x}px) translateY(${y}px)` }}
+    >
+      <CursorSVG color={color} />
+
+      {/* {MESSAGE} */}
+    </div>
+  );
+};
+
+export default Cursor;
