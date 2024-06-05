26666da7b6700611e121f10b1cd4291b8540f453
Oleg Vrabiy <olegvraby@gmail.com>
2024-06-05 19:38:34 +0200

    change:app/page.tsx: The wrapper div's classes has been removed

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+"use client";
+import Live from "@/components/Live";
+
 export default function Page() {
   return (
-    <div className="flex h-[100vh] w-full items-center justify-center text-center">
-      <h1 className="text-2xl text-white">Liveblocks Figma Clone</h1>;
+    <div>
+      <Live />
     </div>
   );
 }
