7481258354437aac1d25c115d384935e8504752c
Oleg Vrabiy <olegvraby@gmail.com>
2024-06-07 14:08:01 +0200

    add:Room: 01.The initial present users into room has been added 02.The Loader has been added

--- app/Room.tsx
+++ app/Room.tsx
@@ -1,15 +1,29 @@
-'use client';
+"use client";
+import { ReactNode } from "react";
+import { LiveMap } from "@liveblocks/client";
 
-import { ReactNode } from 'react';
-import { RoomProvider } from '../liveblocks.config';
-import { ClientSideSuspense } from '@liveblocks/react';
+import { RoomProvider } from "@/liveblocks.config";
+import { ClientSideSuspense } from "@liveblocks/react";
+import Loader from "@/components/Loader";
 
 export function Room({ children }: { children: ReactNode }) {
-   return (
-      <RoomProvider id="my-room" initialPresence={{}}>
-         <ClientSideSuspense fallback={<div>Loading…</div>}>
-            {() => children}
-         </ClientSideSuspense>
-      </RoomProvider>
-   );
+  return (
+    <RoomProvider
+      id="my-room"
+      initialPresence={{
+        cursor: null,
+        cursorColor: null,
+        editingText: null,
+      }}
+      initialStorage={{
+        canvasObjects: new LiveMap(),
+      }}
+    >
+      <ClientSideSuspense fallback={<Loader />}>
+        {() => children}
+      </ClientSideSuspense>
+    </RoomProvider>
+  );
 }
+
+// 2:17:20 stop by time in youtube video
