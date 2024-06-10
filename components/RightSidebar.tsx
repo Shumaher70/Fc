4d237e12ffa9d4a7ef317337ccc19ecf6a38ae05
Oleg Vrabiy <olegvraby@gmail.com>
2024-06-10 12:12:18 +0200

    add:RightSidebar.tsx: The handleInputChange function for modifying shape and getting an attributes for changing value in the rightSidebar

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -4,6 +4,7 @@ import Dimensions from "@/components/settings/Dimensions";
 import Text from "@/components/settings/Text";
 import Color from "@/components/settings/Color";
 import Export from "@/components/settings/Export";
+import { modifyShape } from "@/lib/shapes";
 
 const RightSidebar = ({
   elementAttributes,
@@ -13,6 +14,23 @@ const RightSidebar = ({
   isEditingRef,
   syncShapeInStorage,
 }: RightSidebarProps) => {
+  const handleInputChange = (property: string, value: string) => {
+    if (!isEditingRef.current) isEditingRef.current = true;
+
+    setElementAttributes((prev) => ({
+      ...prev,
+      [property]: value,
+    }));
+
+    modifyShape({
+      canvas: fabricRef.current as fabric.Canvas,
+      property,
+      value,
+      activeObjectRef,
+      syncShapeInStorage,
+    });
+  };
+
   return (
     <section className="min-2-[227px] sticky right-0 flex h-full select-none flex-col border-t border-primary-grey-200 bg-primary-black text-primary-grey-300 max-sm:hidden">
       <h3 className="px-5 pt-4 text-xs uppercase">Design</h3>
@@ -21,7 +39,12 @@ const RightSidebar = ({
         Make changes to canvas as you like
       </span>
 
-      <Dimensions />
+      <Dimensions
+        width={elementAttributes.width}
+        height={elementAttributes.height}
+        handleInputChange={handleInputChange}
+        isEditingRef={isEditingRef}
+      />
       <Text />
       <Color />
       <Color />
